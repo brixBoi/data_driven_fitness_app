12f996457a122297a8d68113cd6d093a23f4fe13
troyam98 <69928520+troyam98@users.noreply.github.com>
2020-09-25 20:05:23 +1200

    Update exercise_log.dart

--- lib/logic/model/completed_activities/exercise_log.dart
+++ lib/logic/model/completed_activities/exercise_log.dart
@@ -1,5 +1,6 @@
 import 'package:data_driven_fitness_app/logic/model/exercise_concepts/exercise.dart';
 
+///class to log details of an exercise that has been attempted
 class ExerciseLog {
   ExerciseLog(this.exercise, this.completedSuccessfully, this.reps, this.sets);
 
